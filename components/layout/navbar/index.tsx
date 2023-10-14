abc97a8117965e91e1e08a4d71bfa44d3cfd89f4
Jason Wiker <Jason.wiker@vercel.com>
2023-10-13 20:38:26 -0600

    desktop header

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -1,34 +1,32 @@
 import Cart from 'components/cart';
 import OpenCart from 'components/cart/open-cart';
 import LogoSquare from 'components/logo-square';
-import { getMenu } from 'lib/shopify';
-import { Menu } from 'lib/shopify/types';
 import Link from 'next/link';
 import { Suspense } from 'react';
 import { client } from '../../../sanity/lib/client';
 import { urlForImage } from '../../../sanity/lib/image';
 
-import MobileMenu from './mobile-menu';
 import Search from './search';
 const { SITE_NAME } = process.env;
 
 export default async function Navbar() {
-  const menu = await getMenu('next-js-frontend-header-menu');
   const nav = await client.fetch(`*[_type == "headerNavigation"]`);
   const navData = nav[0];
   const logoUrl = urlForImage(navData.logo.asset._ref).url();
-  console.log(logoUrl);
+  const navOptions = navData.napOptions.map((x) => {
+    return { title: x.name, path: x.path.current };
+  });
   return (
     <nav className="relative flex items-center justify-between p-4 lg:px-6">
-      <div className="block flex-none md:hidden">
+      {/* <div className="flex-none block md:hidden">
         <MobileMenu menu={menu} />
-      </div>
+      </div> */}
       <div className="flex w-full items-center">
         <div className="flex w-full md:w-1/3">
           <Link href="/" className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6">
             <LogoSquare logoUrl={logoUrl} />
           </Link>
-          {menu.length ? (
+          {/* {menu.length ? (
             <ul className="hidden gap-6 text-sm md:flex md:items-center">
               {menu.map((item: Menu) => (
                 <li key={item.title}>
@@ -41,9 +39,20 @@ export default async function Navbar() {
                 </li>
               ))}
             </ul>
-          ) : null}
+          ) : null} */}
         </div>
-        <div className="hidden justify-end md:flex md:w-1/3"></div>
+        <div className="flex flex-row justify-center gap-2 md:flex md:w-1/3">
+          {navOptions.map((item) => (
+            <div key={item.title}>
+              <Link
+                href={item.path}
+                className="text-xs uppercase tracking-widest text-black underline-offset-4 hover:text-black hover:underline"
+              >
+                {item.title}
+              </Link>
+            </div>
+          ))}
+        </div>
         <div className="flex justify-end gap-4 md:w-1/3">
           <Search />
           <Suspense fallback={<OpenCart />}>
