f4fb91737d221dac97e3cae47726fc75df5b5f5b
Jason Wiker <Jason.wiker@vercel.com>
2023-10-13 20:39:44 -0600

    Update footer.tsx

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 
 import FooterMenu from 'components/layout/footer-menu';
-import LogoSquare from 'components/logo-square';
 import { getMenu } from 'lib/shopify';
 import { Suspense } from 'react';
 
@@ -19,8 +18,8 @@ export default async function Footer() {
       <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-neutral-200 px-6 py-12 text-sm dark:border-neutral-700 md:flex-row md:gap-12 md:px-4 min-[1320px]:px-0">
         <div>
           <Link className="flex items-center gap-2 text-black dark:text-white md:pt-1" href="/">
-            <LogoSquare size="sm" />
-            <span className="uppercase">{SITE_NAME}</span>
+            {/* <LogoSquare size="sm" /> */}
+            {/* <span className="uppercase">{SITE_NAME}</span> */}
           </Link>
         </div>
         <Suspense
