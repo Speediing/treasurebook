9041beb94621059757e1cf0b8c858b2a48beb58e
Jason Wiker <Jason.wiker@vercel.com>
2023-12-20 15:45:08 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ module.exports = {
     // Disabling on production builds because we're running checks on PRs via GitHub Actions.
     ignoreDuringBuilds: true
   },
-  experimental: {},
+  experimental: {
+    ppr: true
+  },
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
