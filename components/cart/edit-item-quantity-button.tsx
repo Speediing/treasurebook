369045c91f0bfc852ddd8bd07eb18209cf2374d5
Jason Wiker <Jason.wiker@vercel.com>
2023-12-22 16:35:13 -0700

    Update edit-item-quantity-button.tsx

--- components/cart/edit-item-quantity-button.tsx
+++ components/cart/edit-item-quantity-button.tsx
@@ -5,6 +5,7 @@ import clsx from 'clsx';
 import { updateItemQuantity } from 'components/cart/actions';
 import LoadingDots from 'components/loading-dots';
 import type { CartItem } from 'lib/shopify/types';
+// @ts-ignore
 import { useFormState, useFormStatus } from 'react-dom';
 
 function SubmitButton({ type }: { type: 'plus' | 'minus' }) {
