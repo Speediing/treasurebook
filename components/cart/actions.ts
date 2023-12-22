720a047d32015b856b483beee581e83dc35744a5
Jason Wiker <Jason.wiker@vercel.com>
2023-12-22 15:45:29 -0700

    .

--- components/cart/actions.ts
+++ components/cart/actions.ts
@@ -6,7 +6,7 @@ import { revalidateTag } from 'next/cache';
 import { cookies } from 'next/headers';
 
 export async function addItem(prevState: any, selectedVariantId: string | undefined) {
-  let cartId = cookies().get('cartId')?.value;
+  let cartId = cookies().get('cartId')?.value || '';
   let cart;
 
   if (cartId) {
