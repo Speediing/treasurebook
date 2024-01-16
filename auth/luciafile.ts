df43429ed6af3c06278108a6a4fa829e0ea88ddd
Jason Wiker <Jason.wiker@vercel.com>
2024-01-16 07:23:19 -0700

    .

--- auth/luciafile.ts
+++ auth/luciafile.ts
@@ -1,11 +1,11 @@
-import lucia from 'lucia';
+import { lucia } from 'lucia';
 import { nextjs_future } from 'lucia/middleware';
 
 import { pg } from '@lucia-auth/adapter-postgresql';
 import { upstash } from '@lucia-auth/adapter-session-redis';
 import { kv } from '@vercel/kv';
 import { db } from '@vercel/postgres';
-export const auth = lucia.lucia({
+export const auth = lucia({
   adapter: {
     user: pg(db, {
       user: 'auth_user',
