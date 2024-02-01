20a2d539dea7007bfb99f68370acf9330c015743
Jason Wiker <Jason.wiker@vercel.com>
2024-02-01 16:45:08 -0700

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,13 +4,23 @@ terraform {
       source = "vercel/vercel"
       version = "~> 0.3"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
 provider "vercel" {
-
-
   # Optional default team for all resources
   team = "team_zfgWnJXv2XUd0rTW2okTYvSZ"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_secretsmanager_secret" "example" {
+  name = "example"
 }
 
 resource "vercel_project" "example" {
