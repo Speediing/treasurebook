1196d8213f3119ae03bcf5a7f8ca6adacc82afa3
Jason Wiker <Jason.wiker@vercel.com>
2024-02-02 16:54:05 -0700

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,4 +1,16 @@
-module "ec2_instances" {
+terraform {
+  required_providers {
+    vercel = {
+      source = "vercel/vercel"
+      version = "~> 0.3"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+module "proejctOne" {
   source  = "./VercelProject"
  
 }
